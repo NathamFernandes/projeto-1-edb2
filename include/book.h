ed88937042b4c97b0c2ba445cb03fb1870a4be6f
Nathan Oliveira <nathanliveira399@gmail.com>
2024-11-21 18:28:51 -0300

    adicao de comentarios

--- include/book.h
+++ include/book.h
@@ -3,31 +3,39 @@
 
 #define MAX_LEN 50
 
+//== Structs
+
+// Representaçãp de um livro.
 typedef struct {
-    unsigned    id;
-    char        title[MAX_LEN];
-    char        author[MAX_LEN];
-    char        genre[MAX_LEN];
-    unsigned    year;
-    char        publisher[MAX_LEN];
-    unsigned    pages;
+    unsigned    id;                     //! Código único de um livro.
+    char        title[MAX_LEN];         //! Titulo de um livro.
+    char        author[MAX_LEN];        //! Autor de um livro.
+    char        genre[MAX_LEN];         //! Gênero de um livro.
+    unsigned    year;                   //! Ano de publicação do livro.
+    char        publisher[MAX_LEN];     //! Editora do livro.
+    unsigned    pages;                  //! Quantidade de páginas do livro.
 } Book;
 
+// Representaçãp de um nó da árvore.
 typedef struct node {
-    Book book;
-    struct node* left;
-    struct node* right;
+    Book book;              //! Dado guardado pelo nó da árvore.
+    struct node* left;      //! Ponteiro para o filho esquerdo do nó.
+    struct node* right;     //! Ponteiro para o filho direito do nó.
 } Node;
 
-typedef struct {
-    Node* root;
-} BTree;
+//== Main functions
 
+// Inicializa uma árvore vazia.
 Node* initialize();
+// Carrega os livros de um arquivo .csv.
 Node* load_books(const char *file_path, Node* root);
+// Insere um livro na árvore.
 void insert(Node** root, const Book book);
+// Busca por todos os livros de um determinado gênero.
 void search_by_genre(Node* root, const char *genre);
+// Lista todos os livros da árvore.
 void show_books(Node* root);
+// Desaloca toda a árvore binária.
 void deallocate(Node* root);
 
 #endif
