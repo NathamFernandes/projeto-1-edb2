0b58da34b15c1a0bf4171c689c543aba4e0a60aa
Nathan Oliveira <nathanoliveira399@gmail.com>
2024-11-30 12:51:15 -0300

    feat: tratamento de linha de comando

--- src/main.c
+++ src/main.c
@@ -6,19 +6,18 @@
 
 int main(int argc, char *argv[])
 {
-    Node *library = initialize();
-
-    library = load_books(argv[1], library);
-
-    if (library == NULL)
-    {
+    if (argc == 1) {
+        printf("Usage: library <file.csv>\n");
         return EXIT_FAILURE;
     }
 
-    main_loop(library);
+    Node *library = initialize();
+    library = load_books(argv[1], library);
 
-    deallocate(library);
-    library = NULL;
+    if (library == NULL)
+        return EXIT_FAILURE;
+
+    main_loop(library);
 
     return EXIT_SUCCESS;
 }
